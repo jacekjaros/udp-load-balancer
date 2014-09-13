5d8b6625afe009cb720d36bcb5abd63a4a91bfe6
Felix Carmona <mail@felixcarmona.com>
2014-09-13 12:32:58 +0200

    added .travis.yml

--- src/argparse.c
+++ src/argparse.c
@@ -1,9 +1,12 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "udp_load_balancer.h"
+
 
 unsigned int is_help_required(int argc, char **argv) {
-    for (unsigned int i = 0; i < argc; ++i)
+    unsigned int i;
+    for (i = 0; i < argc; ++i)
     {
         if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
             return 1;
@@ -17,7 +20,8 @@ unsigned int get_udp_load_balancer_port(int argc, char **argv) {
     char *port_as_string;
     unsigned int port;
 
-    for (unsigned int i = 0; i < argc; ++i)
+    unsigned int i;
+    for (i = 0; i < argc; ++i)
     {
         if(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--port") == 0) {
             port_as_string = argv[i+1];
@@ -36,7 +40,7 @@ unsigned int get_udp_load_balancer_port(int argc, char **argv) {
 
 char **get_servers_list(int argc, char **argv) {
     char **servers_list = NULL;
-    char* servers_list_raw = NULL;
+    char *servers_list_raw = NULL;
     char *token;
     unsigned int i;
     for (i = 0; i < argc; ++i)
@@ -54,13 +58,13 @@ char **get_servers_list(int argc, char **argv) {
     i = 0;
     token = strtok(servers_list_raw, " , ");
     while (token) {
-      servers_list = realloc (servers_list, sizeof (char*) * ++i);
-      servers_list[i-1] = token;
-      token = strtok (NULL, " , ");
+        servers_list = realloc (servers_list, sizeof (char*) * ++i);
+        servers_list[i-1] = token;
+        token = strtok (NULL, " , ");
     }
 
     /* realloc one extra element for the last NULL */
-    servers_list = realloc (servers_list, sizeof (char*) * (i+1));
+    servers_list = realloc(servers_list, sizeof (char*) * (i+1));
     servers_list[i] = 0;
 
     return servers_list;
@@ -68,13 +72,12 @@ char **get_servers_list(int argc, char **argv) {
 
 char **get_servers_hosts(char **servers_list, int servers_amount) {
     char *server_raw, *server_host, serve;
-    char **servers_hosts;
+    char **servers_hosts = malloc(MAX_SERVERS * sizeof(char*));
 
-    for(unsigned int i = 0; i < servers_amount; i++) {
+    unsigned int i;
+    for(i = 0; i < servers_amount; i++) {
         server_raw = strdup(servers_list[i]);
         server_host = strsep(&server_raw, ":");
-
-        servers_hosts = realloc(servers_hosts, sizeof(char*) * i+1);
         servers_hosts[i] = server_host;
     }
 
@@ -83,15 +86,20 @@ char **get_servers_hosts(char **servers_list, int servers_amount) {
 
 int *get_servers_ports(char **servers_list, int servers_amount) {
     char *server_raw, *port_string;
-    int *servers_ports, server_port;
+    int server_port;
+    int *servers_ports = malloc(MAX_SERVERS * sizeof(int*));
 
-    for(unsigned int i = 0; i < servers_amount; i++) {
+    unsigned int i, j;
+    for(i = 0; i < servers_amount; i++) {
         server_raw = strdup(servers_list[i]);
         strsep(&server_raw, ":");
         port_string = strsep(&server_raw, ":");
-        server_port = (int)strtol(port_string, (char **)NULL, 10);
 
-        servers_ports = realloc(servers_ports, sizeof (int*) * i+1);
+        server_port = 0;
+        for(j=0; j < strlen(port_string); j++){
+		    server_port = server_port * 10 + ( port_string[j] - '0' );
+	    }
+
         servers_ports[i] = server_port;
     }
 
